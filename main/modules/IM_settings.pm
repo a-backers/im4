e955cf3eca73587fc18e6268939232fcd9417da0
a-backers <107916060+a-backers@users.noreply.github.com>
2023-06-20 15:26:35 +0200

    m

--- main/modules/IM_settings.pm
+++ main/modules/IM_settings.pm
@@ -1,5 +1,34 @@
 package IM_settings;
 
 use strict;
+require Exporter;
 
+# Settings module for shared perl settings.
+our @ISA     = ("Exporter");
+our @EXPORT  = qw(
+                    %IMparam
+                 );
+our @VERSION = 0.1; 
+
+##### Exported environment settings
+our %IMparam = ();
+
+$IMparam{'IMmanDomain'} = $ENV{'IMDomain'};
+
+if ( $ENV{'IM_BASEDIR'} ) { 
+  $IMparam{'IMbaseDir'} = $ENV{'IM_BASEDIR'}; 
+} else {
+  printDebug(0, "IMbaseDir not set, exiting");
+  exit;
+}
+
+$IMparam{'IMmainDir' = "$IMparam{'IMbaseDir'}/main";
+$IMparam{'IMmainConfigDir'} = "$IMparam{'IMmainDir'}/configs";
+$IMparam{'IMsystemDir'} = "$IMparam{'IMbaseDir'}/system";
+$IMparam{'IMsystemConfigDir'} = "$IMparam{'IMsystemDir'}/cmdb/configs";
+$IMparam{'IMsharedDir'} = "$IMparam{'IMbaseDir'}/shared";
+$IMparam{'IMsharedConfDir'} = "$IMparam{'SMsharedDir'}/cmdb/configs";
+
+
+##### MAIN EXIT
 1;

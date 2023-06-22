d34686d3ca1f312128cfa7f18dcfbb982776f67f
Andre Backers <andre.backers@sogeti.com>
2023-06-22 15:21:42 +0200

    m

--- main/bin/im4.pl
+++ main/bin/im4.pl
@@ -5,6 +5,9 @@ use strict;
 BEGIN {
   my $action = $ARGV[0];
   my $domain = $ARGV[1];
+  my $verbose = $ARGV[2];
+
+
 
   # get the env stuff from the config file.
   my $file = "../configs/im.conf";
@@ -24,11 +27,16 @@ BEGIN {
   my $IMbaseDir = $ENV{'IM4_BASEDIR'} || die "Oops, could not find env for IM_BASEDIR";
   $ENV{'PERL5LIB'} = "$IMbaseDir/main/modules";
   our $IMstarterAction = "$action";
-
-  print("ENV: $ENV{'IMDomain'}\n");
-  print("ENV: $ENV{'IM4_BASEDIR'}\n");
-  print("ENV: $ENV{'IM4_HELPDIR'}\n");
-  print("ENV: $ENV{'PERL5LIB'}\n");
 }
+
+my ( $progName, $progOption );
+
+print("ENV: $ENV{'IMDomain'}\n");
+print("ENV: $ENV{'IM4_BASEDIR'}\n");
+print("ENV: $ENV{'IM4_HELPDIR'}\n");
+print("ENV: $ENV{'PERL5LIB'}\n");
+
+print("VAR: $progName\n");
+print("VAR: $progOption\n");
 
 0;

d7f9473fbb029e305e3a7d4b02b917d2f1f77455
Andre Backers <andre.backers@sogeti.com>
2023-06-22 15:07:55 +0200

    read file test

--- main/bin/im4.pl
+++ main/bin/im4.pl
@@ -3,6 +3,16 @@
 use strict;
 
 BEGIN {
+  my $action = $ARGV[0];
+  my $domain = $ARGV[1];
+
+  print("BEGIN: Read CONF file");
+  # get the env stuff from the config file.
+  my $file = "../configs/im.conf";
+  open (IN, $file) || die("BEGIN: cannot open $file.");
+  my @lines = <IN>;
+  close IN;
+
   $ENV{'IMDomain'} = "TEST-AB";
 
   print("ENV: $ENV{'IMDomain'}\n");

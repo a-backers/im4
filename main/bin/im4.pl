ed42d086e3d3b51a18b4bafe05353ec488c49773
Andre Backers <andre.backers@sogeti.com>
2023-06-22 15:10:30 +0200

    fill env

--- main/bin/im4.pl
+++ main/bin/im4.pl
@@ -6,12 +6,23 @@ BEGIN {
   my $action = $ARGV[0];
   my $domain = $ARGV[1];
 
-  print("BEGIN: Read CONF file");
+  print("BEGIN: Read CONF file\n");
   # get the env stuff from the config file.
   my $file = "../configs/im.conf";
   open (IN, $file) || die("BEGIN: cannot open $file.");
   my @lines = <IN>;
   close IN;
+
+  print("BEGIN: Fill env; in loop");
+  foreach my $regel (@lines) {
+    chomp $regel;
+    my ( $varName, $info ) = split('=', $regel);
+    if ( substr($varName, 0, 1) ne "#" ) {
+      $ENV{$varName} = $info;
+      print("ENV Line= $info");
+    }
+  }
+  
 
   $ENV{'IMDomain'} = "TEST-AB";
 

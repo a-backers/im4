d1dbf14fa33effd7f2954b48a08443189418a6c1
Andre Backers <andre.backers@sogeti.com>
2023-06-22 16:33:03 +0200

    m

--- main/modules/IM_base.pm
+++ main/modules/IM_base.pm
@@ -8,7 +8,7 @@ require Exporter;
 # Settings module for shared perl settings.
 our @ISA     = ("Exporter");
 our @EXPORT  = qw(
-                    printDebug
+                    printDate printDebug
                  );
 our @VERSION = 0.1; 
 
@@ -26,5 +26,34 @@ sub printDebug {
     } else {
       print("<BR>   <B>$dateStr: $string</B> ");
     }
+  }
+}
+
+sub printDate {
+  my $timestamp = shift;
+  my $paramType = shift;
+  my $type = "default";
+
+  if (  "$paramType" ne "" ) {
+    $type = $paramType;
+  }
+
+  my ($sec,$min,$hour,$mday,$mon,$jaar,$wday,$yday,$isdst) = localtime($timestamp);
+  my @month = qw(dummy Jan Feb Mar Apr May Jun Jul Aug Sep Okt Nov Dec);
+  my @z2 = ('00' .. '60');
+
+  $mon++;
+  $jaar = $jaar + 1900;
+
+  if ( $type eq "yyyymmdd" ) {
+    return("$jaar\-$z2[$mon]\-$z2[$mday]");
+  } elsif ( $type eq "hhmmss" ) {
+    return("$z2[$hour]\:$z2[$min]\:$z2[$sec]");
+  } elsif ( $type eq "logdate" ) {
+    return("$jaar\-$z2[$mon]\-$z2[$mday]_$z2[$hour]\:$z2[$min]\:$z2[$sec]");
+  } elsif ( $type eq "wday" ) {
+    return("$wday");
+  } else {
+    return("$jaar\-$z2[$mon]\-$z2[$mday] $z2[$hour]\:$z2[$min]\:$z2[$sec]");
   }
 }
